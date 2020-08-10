a31b4e3ad2f6ece235c447e016ebc33bcb0b4a2b
tylermarshall7 <tyler.marshall7@gmail.com>
2020-08-10 11:09:00 -0600

    update

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "ddev-jgxr65m3.us.auth0.com";
+export const audience = "http://bug-report-api.com";
+export const clientId = "Zk2TNjakxQLyASeedlyip8uYFfgv3vp6J";
